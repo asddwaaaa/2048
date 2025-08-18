1a4980d178505a1aae68db4ef26e9389a7c04158
asddwaaaa <qwertyboominga@gmail.com>
2025-08-18 14:18:56 +0500

    Update app.js

--- 2048-game/app.js
+++ 2048-game/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
@@ -10,4 +10,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
 });
